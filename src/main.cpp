c9bc97bde62aa8ffaae8f54a4f54f23092db4654
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2023-11-06 08:16:56 +0900

    added smpte color bars as photo

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, const char *argv[])
 {
-    Image test("./assets/photo.jpg");
+    Image test("./assets/photo.png");
     // Image gray_average = test;
     // gray_average.grayscale_lum();
     // gray_average.write("./assets/photo_grayscale_lum.png");
